93dfe04f901697c3494ed7f1550a47f68f6ab01c
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-04-14 16:43:06 +0300

    Redone splitting for the original data

--- convert_original_data.py
+++ convert_original_data.py
@@ -3,7 +3,7 @@ import json
 from typing import List, Tuple
 from pathlib import Path
 
-import numpy as np
+from sklearn.model_selection import train_test_split
 
 from constants import SEP_TOKEN, ORIGINAL_LEMMAS_PATHS
 
@@ -26,30 +26,33 @@ def write_examples(path: str, data: List[Tuple[str, str]]):
 
 def main():
     Path("data/converted_lemmas").mkdir(parents=True, exist_ok=True)
+    examples = []
+    has_splits = []
     for key, value in ORIGINAL_LEMMAS_PATHS.items():
-        if key == "train":
-            with open(f"data/converted_lemmas/lemmas_{key}.jsonl", "w") as out_file:
-                with open(value) as in_file:
-                    index = 1
-                    for line in in_file:
-                        original, segmented = line.split("\t")
-                        data = {"index": index,
-                                "original": original,
-                                "segmented": remove_labels(segmented)}
-                        index += 1
-                        out_file.write(json.dumps(data, ensure_ascii=False) + "\n")
-        else:
-            examples = []
-            with open(value) as in_file:
-                for line in in_file:
-                    original, segmented = line.split("\t")
-                    examples.append((original, segmented))
-            split = int(np.floor(len(examples) / 2))
-            valid = examples[:split]
-            test = examples[split:]
+        with open(value) as in_file:
+            for line in in_file:
+                original, segmented = line.split("\t")
+                segmented = remove_labels(segmented)
+                examples.append((original, segmented))
+                has_splits.append(SEP_TOKEN in segmented)
 
-            write_examples(f"data/converted_lemmas/lemmas_valid.jsonl", valid)
-            write_examples(f"data/converted_lemmas/lemmas_test.jsonl", test)
+    examples_train, examples_test, has_splits_train, has_splits_test = train_test_split(examples,
+                                                                                        has_splits,
+                                                                                        test_size=0.3,
+                                                                                        random_state=42,
+                                                                                        shuffle=True,
+                                                                                        stratify=has_splits)
+
+    examples_valid, examples_test, _, _ = train_test_split(examples_test,
+                                                           has_splits_test,
+                                                           test_size=0.5,
+                                                           random_state=42,
+                                                           shuffle=True,
+                                                           stratify=has_splits_test)
+
+    write_examples(data=examples_train, path="data/converted_lemmas/lemmas_train.jsonl")
+    write_examples(data=examples_valid, path="data/converted_lemmas/lemmas_valid.jsonl")
+    write_examples(data=examples_test, path="data/converted_lemmas/lemmas_test.jsonl")
 
 
 if __name__ == "__main__":
