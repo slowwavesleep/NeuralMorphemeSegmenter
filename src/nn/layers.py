Bug fixes and changes to loss calculation

src/nn/layers.py
@@ -83,10 +83,8 @@ class LstmEncoderPacked(LstmEncoder):
 
         self.bidirectional = bidirectional
 
-    def forward(self, encoder_seq):
+    def forward(self, encoder_seq: torch.tensor, true_lengths: torch.tensor):
         initial_len = encoder_seq.size(-1)
-
-        encoder_lens = get_non_pad_lens(encoder_seq)
 
         encoder_seq = self.embedding(encoder_seq)
 
@@ -95,7 +93,7 @@ class LstmEncoderPacked(LstmEncoder):
         encoder_seq = self.layer_norm(encoder_seq)
 
         encoder_seq = pack_padded_sequence(input=encoder_seq,
-                                           lengths=encoder_lens,
+                                           lengths=true_lengths,
                                            batch_first=True,
                                            enforce_sorted=False)
 

src/nn/training_process.py
@@ -30,9 +30,9 @@ def train(model: Module,
         target_seq = target_seq.to(device).squeeze(0)
         true_lens = true_lens.squeeze(0)
 
-        batch_size = encoder_seq.size(0)
-        avg_tokens = torch.sum(true_lens) / batch_size
-        loss = model(encoder_seq, target_seq) / avg_tokens
+        loss = model(encoder_seq, target_seq, true_lens)
+        # loss = loss.view(encoder_seq.size(0), encoder_seq.size(1))
+        # loss = torch.mean(torch.sum(loss, axis=1))
 
         optimizer.zero_grad()
         loss.backward()
@@ -71,8 +71,8 @@ def evaluate(model: Module,
         true_lens = true_lens.squeeze(0)
 
         with torch.no_grad():
-            loss = model(encoder_seq, target_seq)
-            preds = model.predict(encoder_seq)
+            loss = model(encoder_seq, target_seq, true_lens)
+            preds = model.predict(encoder_seq, true_lens)
 
         losses.append(loss.item())
 

src/utils/segmenters.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
+import torch
 from torch import nn
 from torch import Tensor
 
@@ -48,6 +49,7 @@ class RandomSegmenter(AbstractSegmenter):
     def tag_example(self, example: str):
         encoded = self.original_tokenizer.encode(example)
         encoded = Tensor(encoded).long().unsqueeze(0)
+
         prediction = self.tagger.predict(encoded).squeeze(0).tolist()
         prediction = self.bmes_tokenizer.decode(prediction)
         return prediction
@@ -107,9 +109,12 @@ class NeuralSegmenter(AbstractSegmenter):
         self.sep = sep
 
     def tag_example(self, example):
+
+        true_length = torch.tensor(len(example)).long().to(self.device).unsqueeze(0)
+
         encoded = self.original_tokenizer.encode(example)
-        encoded = Tensor(encoded).long().unsqueeze(0).to(self.device)
-        prediction = self.tagger.predict(encoded).squeeze(0).tolist()
+        encoded = torch.tensor(encoded).long().unsqueeze(0).to(self.device)
+        prediction = self.tagger.predict(encoded, true_length).squeeze(0).tolist()
         prediction = self.bmes_tokenizer.decode(prediction)
         return prediction
 
@@ -120,6 +125,8 @@ class NeuralSegmenter(AbstractSegmenter):
 
     def tag_batch(self, example_batch: List[str]):
         true_lengths = [len(example) for example in example_batch]
+        true_lengths = torch.tensor(true_lengths).long().to(self.device).unsqueeze(0)
+
         max_length = max(true_lengths)
 
         processed_examples = []
@@ -129,8 +136,8 @@ class NeuralSegmenter(AbstractSegmenter):
             padded = self.original_tokenizer.pad_or_clip(encoded, max_len=max_length)
             processed_examples.append(padded)
 
-        processed_examples = Tensor(processed_examples).long().to(self.device)
-        predictions = self.tagger.predict(processed_examples).tolist()
+        processed_examples = torch.tensor(processed_examples).long().to(self.device)
+        predictions = self.tagger.predict(processed_examples, true_lengths).tolist()
 
         return predictions, true_lengths
 