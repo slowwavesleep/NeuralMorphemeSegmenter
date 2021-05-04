72f6ce796fdf8275fd9d0da3a41a5a3cdb32a647
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-05-04 14:58:21 +0300

    Updated script

--- run_experiment_job.sh
+++ run_experiment_job.sh
@@ -2,12 +2,14 @@
 
 file_name=$1
 num_runs=$2
+visible_devices=$3
 
 while read line;
 do
   for (( c=1; c<=$num_runs; c++ ))
   do
     echo "Experiment run $c"
-    python run_experiment.py -p $line
+    echo $visible_devices
+    CUDA_VISIBLE_DEVICES=$visible_devices python run_experiment.py -p $line
   done
 done < $file_name
