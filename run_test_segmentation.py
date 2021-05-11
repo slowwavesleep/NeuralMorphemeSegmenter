fdd43fd15e0d480b297eed77c7a1635873603ec5
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-05-11 22:07:41 +0300

    Revert fix

--- run_test_segmentation.py
+++ run_test_segmentation.py
@@ -206,8 +206,7 @@ with open(write_path, "w") as file:
                                                                                   segmented_words,
                                                                                   predictions,
                                                                                   true_lengths):
-        # TODO fix this properly
-        original_word = original_tokenizer.decode(original_word)[:true_length].replace("<UNK>", "ё")
+        original_word = original_tokenizer.decode(original_word)[:true_length]
         target_bmes = bmes_tokenizer.decode(target_segmentation)[:true_length]
         predicted_bmes = bmes_tokenizer.decode(prediction)[:true_length]
 
